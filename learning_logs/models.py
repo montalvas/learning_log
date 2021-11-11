ee2a64ec93f9ad4a872922c252b3f51a00a583f1
Montalvas <lucas_montalvani@hotmail.com>
2021-11-11 08:02:17 -0300

    Modelo de entrada

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -7,5 +7,19 @@ class Topic(models.Model):
     data_added = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
-        """Devolve uma representação em string do modelo."""
+        """Retorna o nome do tópico"""
         return self.text
+
+
+class Entry(models.Model):
+    """Algo específico aprendido sobre um assunto."""
+    topic = models.ForeignKey(Topic)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = 'entries'
+    
+    def __str__(self):
+        """Retorna até 50 caracteres do conteúdo."""
+        return self.text[:50] + "..."
